d5d62b68a7a3499fae3744483a8016615442ceb2
Rebeca Cabañas Arauz <beckyarauz@users.noreply.github.com>
2018-11-25 22:12:34 +0100

    Updates

---
+++ precache-manifest.da314f6ec56d58f62e80fa65d349e214.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Test-App/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d9025a24cdadf44f3141",
+    "url": "/Test-App/static/js/runtime~main.d9025a24.js"
+  },
+  {
+    "revision": "de257ca80a211d79a18e",
+    "url": "/Test-App/static/js/main.de257ca8.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/Test-App/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "de257ca80a211d79a18e",
+    "url": "/Test-App/static/css/main.b97b1e07.chunk.css"
+  },
+  {
+    "revision": "4270d6cf07eff8e050cbbd812b85aeb2",
+    "url": "/Test-App/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/beckyarauz/Test-App/precache-manifest.892304882b71907d4e1579f6776e1853.js"
+  "/Test-App/precache-manifest.da314f6ec56d58f62e80fa65d349e214.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/beckyarauz/Test-App/index.html", {
+workbox.routing.registerNavigationRoute("/Test-App/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
