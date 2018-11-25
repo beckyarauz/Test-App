36b065e037bece17bd4c703e931d3fac73b0279a
Rebeca Cabañas Arauz <beckyarauz@users.noreply.github.com>
2018-11-25 21:19:21 +0100

    Updates

---
+++ precache-manifest.892304882b71907d4e1579f6776e1853.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/beckyarauz/Test-App/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "851175b79a838f590511",
+    "url": "/beckyarauz/Test-App/static/js/runtime~main.851175b7.js"
+  },
+  {
+    "revision": "076275e7d10de2d3de30",
+    "url": "/beckyarauz/Test-App/static/js/main.076275e7.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/beckyarauz/Test-App/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "076275e7d10de2d3de30",
+    "url": "/beckyarauz/Test-App/static/css/main.b97b1e07.chunk.css"
+  },
+  {
+    "revision": "33b5a6afee4cb4a7fa75e4be892acd0a",
+    "url": "/beckyarauz/Test-App/index.html"
+  }
+];
